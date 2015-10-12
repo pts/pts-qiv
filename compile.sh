dee85ca2b1b79f45cf4da53ae5f24d0f0892b340
Peter Szabo <pts@fazekas.hu>
2015-10-12 07:12:15 +0000

    added more compilation commands

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
-#! /bin/bash
+#! /bin/sh --
 # on Ubuntu Karmic
+# on Ubuntu Lucid: sudo apt-get install libimlib2-dev libmagic-dev
 # on Ubuntu Trusty: sudo apt-get install libexif-dev libmagic-dev libimlib2-dev libgtk2.0-dev
 
 make qiv
